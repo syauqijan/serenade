d803b43e8907300d17effe2411d4e7e34d9cb08b
syauqijan <13521014@std.stei.itb.ac.id>
2024-01-28 22:31:35 +0700

    style: main song

--- src/components/mainSong.tsx
+++ src/components/mainSong.tsx
@@ -1,15 +1,7 @@
 import React, { useState } from "react";
 
 const Song = ({ currentSong }: { currentSong: any }) => {
-    // const currentSong = {
-    // name: "Song Name",
-    // artist: "Artist Name",
-    // cover: "/images/cover.jpg",
-    // audio: "/audio/1.mp3",
-    // color: ["#205950", "#2ab3bf"],
-    // id: 1,
-    // active: true,
-    // };
+
   return (
     <div
       className="bg-no-repeat bg-cover bg-bottom bg-[#662fb3] flex flex-col items-center justify-center min-h-[30rem] gap-7"
@@ -18,10 +10,9 @@ const Song = ({ currentSong }: { currentSong: any }) => {
       }}
     >
       <img className="cover-art w-1/5 h-full object-cover" src={currentSong.cover} alt="Cover Art" />
-      {/* <div className="flex flex-col justify-center items-center "> */}
-            <h2 className="text-2xl">{currentSong.name}</h2>
+            <h2 className="text-3xl font-extrabold">{currentSong.name}</h2>
             <h3 className="text-xl">{currentSong.artist}</h3>
-      {/* </div> */}
+
       
     </div>
   );
--- src/components/songs.tsx
+++ src/components/songs.tsx
@@ -71,7 +71,7 @@ const songs : FC<SongProps> = ({song, currentSong, setSongs, setCurrentSong, isP
     }
 
   return (
-    <div className='h-32 flex flex-row ml-8 mr-8 mt-3 hover:bg-sky-700 hover:rounded-md cursor-pointer hover:transition-colors' onClick={songHandler}>
+    <div className='h-32 flex flex-row ml-8 mr-8 mt-3 hover:bg-primary hover:rounded-md cursor-pointer hover:transition-colors ' onClick={songHandler}>
         <img className='w-24 h-24 ml-6 mt-4 mb-4  justify-center  rounded-md object-cover' src={song.cover} alt="Cover Art" />
         <div className='pl-5 justify-center flex flex-col'>
             <h2 className='text-lg'>{song.name}</h2>
